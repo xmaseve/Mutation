656f93a911c20f9ec39ae6bfcedfc7cbd9ca2ee9
Yi Qi <qiyi1224@gmail.com>
2016-04-04 00:16:29 -0400

    Update mutation.py

--- mutation.py
+++ mutation.py
@@ -5,6 +5,7 @@ Created on Mon Apr 04 00:07:15 2016
 @author: YI
 """
 
+#list is mutable
 def add_to(num, target=[]):
     target.append(num)
     return target
@@ -17,6 +18,7 @@ add_to(2)
 
 add_to(3)
 # Output: [1, 2, 3]
+
 
 def add_to(element, target=None):
     if target is None:
